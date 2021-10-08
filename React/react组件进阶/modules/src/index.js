296bacd05ae15c7e330490727088620fe0fc800f
fatcheesecurtilage <1053082770@qq.com>
2021-10-08 21:38:47 +0800

    no message

--- React/react组件进阶/modules/src/index.js
+++ React/react组件进阶/modules/src/index.js
@@ -189,45 +189,181 @@ import  ReactDOM, { render }  from 'react-dom'
 */
 
 //创建context得到两个组件
-const {Provider,Consumer} = React.createContext()
+// const {Provider,Consumer} = React.createContext()
 
-class App extends React.Component{
-  render(){
-    return(
-      <Provider value="pink">
-        <div>
-          <Node />
-        </div>
-      </Provider>
-    )
-  }
-}
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <Provider value="pink">
+//         <div>
+//           <Node />
+//         </div>
+//       </Provider>
+//     )
+//   }
+// }
 
-const Node = props => {
-  return (
+// const Node = props => {
+//   return (
+//     <div>
+//       <SubNode />
+//     </div>
+//   )
+// }
+
+// const SubNode = props => {
+//   return (
+//     <div>
+//       <Child />
+//     </div>
+//   )
+// }
+
+// const Child = props => {
+//   return <div>
+//     <Consumer>
+//       {
+//         data => <span>我是子节点 -- {data}</span>
+//       }
+//     </Consumer>
+//   </div>
+// }
+
+
+
+
+/*
+* children属性
+*/
+
+
+// const App = props =>{
+//   console.log(props)
+//   props.children()
+//   return(
+//     <div>
+//       <h1>组件标签的子节点</h1>
+//       {/* {props.children} */}
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <App>
+//     {() => console.log('这是一个函数子节点')}
+//   </App>,
+//   document.getElementById('root')
+// )
+
+//children为jsx或组件
+//const Test = () => <button>我是button组件</button>
+// const App = props =>{
+//   console.log(props)
+//   return(
+//     <div>
+//       <h1>组件标签的子节点</h1>
+//       {props.children}
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <App>
+//     <p>我是子节点，是一个p标签</p>
+//     <Test/>
+//   </App>,
+//   document.getElementById('root')
+// )
+
+
+
+//children为文本节点
+// const App = props =>{
+//   console.log(props)
+//   return(
+//     <div>
+//       <h1>组件标签的子节点</h1>
+//       {props.children}
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App>我是子节点</App>,document.getElementById('root'))
+
+
+/*
+* props校验
+*/
+
+
+// import PropTypes from 'prop-types'
+
+// const App = props =>{
+//   const arr = props.colors
+//   const lis = arr.map((item,index) => <li key={index}>{item.index}</li>)
+//   return <ul>{lis}</ul>
+// }
+
+// //添加props校验
+// App.propTypes = {
+//   colors:PropTypes.array
+// }
+
+// ReactDOM.render(<App colors={19}></App>,document.getElementById('root'))
+
+
+
+// /*
+// * props常用规则校验
+// */
+
+// import PropTypes from 'prop-types'
+
+// const App = props => {
+//   return(
+//     <div>
+//       <h1>props校验：</h1>
+//     </div>
+//   )
+// }
+
+// //添加props校验
+// //属性a的类型：                       数值(number)
+// //属性fn的类型：                      函数(func)并且为必填项
+// //属性tag的类型：                     React元素(element)
+// //属性filter的类型：                  对象({area:'上海'，price：1999})
+// App.PropTypes = {
+//   a:PropTypes.number,
+//   fn:PropTypes.func.isRequired,
+//   tag:PropTypes.element,
+//   filter:PropTypes.shape({
+//     area:PropTypes.string,
+//     price:PropTypes.number
+//   })
+// }
+
+
+// ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+
+/*
+* props默认值
+*/
+
+const App = props =>{
+  console.log(props)
+  return(
     <div>
-      <SubNode />
+      <h1>此处展示props的默认值:{props.pageSize}</h1>
     </div>
   )
 }
 
-const SubNode = props => {
-  return (
-    <div>
-      <Child />
-    </div>
-  )
+//添加props默认值
+App.defaultProps = {
+  pageSize:10
 }
 
-const Child = props => {
-  return <div>
-    <Consumer>
-      {
-        data => <span>我是子节点 -- {data}</span>
-      }
-    </Consumer>
-  </div>
-}
-ReactDOM.render(<App />,document.getElementById('root'))
-
-
+ReactDOM.render(<App/>,document.getElementById('root'))
